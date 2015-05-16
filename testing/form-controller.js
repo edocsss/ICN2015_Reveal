55c43000296c071ab80396373e13cd1ace8ee728
edwin-candinegara <edwin.candinegara@gmail.com>
2015-05-16 22:45:59 +0800

    jQuery mobile and bootstrap compatibility fix (only use needed parts of jQuery mobile)

--- testing/form-controller.js
+++ testing/form-controller.js
@@ -1,7 +1,7 @@
 // Form controller
 $(document).ready(function () {
     // Hide the loading indicator by jQuery UI
-    $("div.ui-loader").hide();
+    //$("div.ui-loader").hide();
 	
     // Hiding loading layer on document ready
     $(".loading-layer").fadeOut('500');

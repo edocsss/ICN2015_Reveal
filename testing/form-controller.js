247c7fc3b53f46cd0dbac605db21a507db764618
michjk <michaelj.kosasih@gmail.com>
2015-05-17 00:24:21 +0800

    Changes in form-ticketing for more responsive & using dropdown menu for selecting quantity of tickets.

--- testing/form-controller.js
+++ testing/form-controller.js
@@ -1,11 +1,8 @@
 // Form controller
 $(document).ready(function () {
-    // Hide the loading indicator by jQuery UI
-    $("div.ui-loader").hide();
-	
-    // Hiding loading layer on document ready
+    
+	// Hiding loading layer on document ready
     $(".loading-layer").fadeOut('500');
-	
 	
 });
 
